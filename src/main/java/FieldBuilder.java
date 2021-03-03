86601fe4f0d9e07d3b1fd232177e8ae362ace553
y-du <42994541+y-du@users.noreply.github.com>
2021-03-03 15:53:44 +0100

    init

---
+++ src/main/java/FieldBuilder.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2021 InfAI (CC SES)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class FieldBuilder {
+
+    private final Map<String, String> patternMap;
+
+    public FieldBuilder(String fieldPatterns) {
+        patternMap = parseFieldPatterns(fieldPatterns);
+    }
+
+    private Map<String, String> parseFieldPatterns(String str) {
+        Map<String, String> patternMap = new HashMap<>();
+        String[] patterns = str.split(",");
+        for (String pattern : patterns) {
+            String[] parts = pattern.split(":");
+            patternMap.put(parts[0], parts[1]);
+        }
+        return patternMap;
+    }
+
+    public String buildField(String rootField, Map<?, ?> data) {
+        String field = patternMap.get(rootField);
+        for (Object key : data.keySet()) {
+            String value;
+            if (data.get(key) instanceof Number) {
+                value = String.valueOf(data.get(key));
+                value = value.replaceAll("\\.\\d+", "");
+            } else {
+                value = String.valueOf(data.get(key));
+            }
+            field = field.replaceAll("\\{" + key + "}", value);
+        }
+        return field;
+    }
+
+    public Set<String> rootFields() {
+        return patternMap.keySet();
+    }
+
+}
